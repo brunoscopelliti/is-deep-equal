62959fcc8da371293be049bb754638c21f97e1e5
Bruno Scopelliti <bru.scopelliti@gmail.com>
2016-01-20 05:33:46 +0100

    id-deep-equal

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 
-
-
 /*
  * @name type
  * @function
  * @private
- * @description Return the type of the input value; it fix the problem with "null", and brings consistence.
+ * @description Return the type of the input value; it fix the problem with "null", and brings consistence (array, date ...).
  */
-const type = subject => Object.prototype.toString.call(subject).toLowerCase();
+const type = subject => Object.prototype.toString.call(subject).toLowerCase().match(/ ([\w]+)/)[1];
+
+
+/*
+ * @name getOwnPropNames
+ * @function
+ * @private
+ * @description Return the own properties of the input parameter.
+ */
+const getOwnPropNames = obj => Object.getOwnPropertyNames.call(null, obj);
+
+
+/*
+ * @name getOwnPropSymbols
+ * @function
+ * @private
+ * @description Return the own symbols of the input parameter.
+ */
+const getOwnPropSymbols = obj => Object.getOwnPropertySymbols.call(null, obj);
+
+
+/*
+ * @name getPrototypeOf
+ * @function
+ * @private
+ * @description Return the prototype object of the input parameter.
+ */
+const getPrototypeOf = obj => Object.getPrototypeOf.call(null, obj);
 
 
 
@@ -22,7 +47,6 @@ const type = subject => Object.prototype.toString.call(subject).toLowerCase();
  */
 export default function deepEqual(valA, valB){
 
-  // 1)
   // if the input values have the same primitive value,
   // or are the same reference,
   // then they are deep equals.
@@ -30,11 +54,38 @@ export default function deepEqual(valA, valB){
     return true;
   }
 
-  // 2)
   // if the input values have different type, or they are primitives
-  // in force of the check (1) here we can return "false"
+  // in force of the previouse check, here we can return "false"
   if (type(valA) != type(valB) || typeof valA != 'object'){
     return false;
+  }
+
+
+  // if the execution arrives here we can be sure
+  // that the two values have the same type
+
+  const propsEqual = () => getOwnPropNames(valA).concat(getOwnPropSymbols(valA)).every(prop => deepEqual(valA[prop], valB[prop]));
+
+
+  if (Array.isArray(valA)){
+
+    // when they are both arrays
+    // in order to be considered deep equals
+    // they should have the same length, and the same element
+    if (valA.length != valB.length || !propsEqual()){
+      return false;
+    }
+
+  }
+  else {
+
+    // in case they are object,
+    // their own properties (key, and symbol) should be deep equals, and also their inherithed properties should be deep equals.
+    if (!deepEqual(getOwnPropNames(valA).sort(), getOwnPropNames(valB).sort()) ||
+      !deepEqual(getOwnPropSymbols(valA).sort(), getOwnPropSymbols(valB).sort()) || !propsEqual() || !deepEqual(getPrototypeOf(valA), getPrototypeOf(valB))){
+        return false;
+    }
+
   }
 
 
--- tests/index.js
+++ tests/index.js
@@ -25,3 +25,119 @@ tape('check primitives type', function(t) {
 
 });
 
+
+
+tape('> array', function(t) { t.end(); });
+
+tape('* same array instance', function(t) {
+  var arr = [1, 2];
+  t.ok(deepEqual(arr, arr), 'same reference');
+  t.end();
+});
+
+tape('* equal array (with symbol)', function(t) {
+
+  t.ok(deepEqual([1, 'b', 'iii'], [1, 'b', 'iii']), 'equal array');
+
+  var s = Symbol();
+
+  var arr1 = [1, 2];
+  var arr2 = [1, 2];
+
+  arr1[s] = arr2[s] = '3';
+  t.ok(deepEqual(arr1, arr2), 'equal array with symbol');
+
+  t.end();
+
+});
+
+tape('* different length', function(t) {
+  t.ok(!deepEqual([1, 2], [1, 2, 3]), 'different arrays');
+  t.end();
+});
+
+tape('* different order', function(t) {
+  t.ok(!deepEqual([1, 2, 3], [1, 3, 2]), 'different arrays');
+  t.end();
+});
+
+tape('* different symbols', function(t) {
+
+  var arr1 = [1, 2];
+  var arr2 = [1, 2];
+
+  arr1[Symbol()] = arr2[Symbol()] = '3';
+  t.ok(!deepEqual(arr1, arr2), 'different symbol, means different arrays');
+
+  t.end();
+
+});
+
+
+
+
+tape('> object', function(t) { t.end(); });
+
+tape('* same object instance', function(t) {
+  var obj = { val: 100 };
+  t.ok(deepEqual(obj, obj), 'same reference');
+  t.end();
+});
+
+tape('* object with same props (order does not matter)', function(t) {
+  var now = new Date();
+  t.ok(deepEqual({ v: 10, now: now, me: 'Bruno', submodel: { active: true } }, { me: 'Bruno', v: 10, now: now, submodel: { active: true } }), 'same properties');
+  t.end();
+});
+
+tape('* complex object', function(t) {
+  t.ok(deepEqual({ v: 10, arr: [1, 2, 3], sub: { arr: [1,'a',true,2], arrObj: [ {v:1}, {v:2, flag: true, sub: { sub1: {v:1} }} ] }},
+    { v: 10, arr: [1, 2, 3], sub: { arr: [1,'a',true,2], arrObj: [ {v:1}, {v:2, flag: true, sub: { sub1: {v:1} }} ] }}), 'same properties');
+  t.end();
+});
+
+tape('* nested property difference', function(t) {
+  t.ok(!deepEqual({ v: 1, sub1: { v: 10, sub2: {enabled: true} } }, { v: 1, sub1: { v: 10, sub2: {enabled: false} } }), 'there is a different property');
+  t.ok(!deepEqual({ v: 1, sub1: { v: 10, sub2: {[Symbol()]: true} } }, { v: 1, sub1: { v: 10, sub2: {[Symbol()]: false} } }), 'there is a different symbol');
+  t.end();
+});
+
+tape('* complex object difference', function(t) {
+  t.ok(!deepEqual({ v: 10, arr: [1, 2, 3], sub: { arr: [1,'a',true,2], arrObj: [ {v:1}, {v:2, flag: true, sub: { sub1: {v:1} }} ] }},
+    { v: 10, arr: [1, 2, 3], sub: { arr: [1,'a',true,2], arrObj: [ {v:1}, {v:2, flag: true, sub: { sub1: {v:2} }} ] }}), 'nested array difference');
+  t.end();
+});
+
+tape('* different instance value', function(t) {
+
+  function A(name) {
+    this.val = name;
+  }
+
+  var a1 = new A('foo');
+  var a2 = new A('bar');
+
+  t.ok(!deepEqual(a1, a2), 'different instance value');
+  t.end();
+});
+
+tape('* different prototype', function(t) {
+
+  function A(name) {
+    this.val = name;
+  }
+
+  A.prototype.noop = function() {};
+
+  function B(name) {
+    this.val = name;
+  }
+
+  B.prototype.noop = function() {};
+
+  var a = new A('foo');
+  var b = new B('foo');
+
+  t.ok(!deepEqual(a, b), 'different prototype');
+  t.end();
+});
